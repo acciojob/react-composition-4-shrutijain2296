7d96a0776e6e8ffd133ca722b36ac17deed6ab48
shrutijain2296 <shrutijain2296@gmail.com>
2023-08-04 18:01:43 +0530

    submitting code-Fri Aug 04 2023 18:01:42 GMT+0530 (India Standard Time)

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+const Form = () =>{
+    let [name, setName] = useState("");
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+
+    function formSubmit(event){
+       event.preventDefault();
+
+       const obj = {
+        name: name,
+        email: email,
+        password: password
+       }
+    }
+
+
+    return(
+        <div>
+            <form onSubmit = {formSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type = "text" id = "name" value = {name} onChange = {(event) => setName(event.target.value)}/>
+                <label htmlFor="email">Email</label>
+                <input type = "email" id = "email" value = {email} onChange = {(event) => setEmail(event.target.value)}/>
+                <label htmlFor="password">Password</label>
+                <input type = "password" id = "password" value = {password} onChange = {(event) => setPassword(event.target.value)}/>
+                <button type = "submit">Submit</button>
+            </form>
+           
+
+        </div>
+    )
+}
+export default Form;
